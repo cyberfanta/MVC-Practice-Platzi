c3363231efeb423af2596157e55ed538b7247b81
Julio César León <julioleon2004@gmail.com>
2021-04-15 00:47:36 -0400

    Feat (init): Beginning this practice for mvc architecture
    Created the model structure and the api key encrypting.

---
+++ app/src/main/java/com/cyberfanta/mvcpracticeplatzi/views/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.cyberfanta.mvcpracticeplatzi.views
+
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.cyberfanta.mvcpracticeplatzi.R
+import com.cyberfanta.mvcpracticeplatzi.models.CryptoUtils
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        Log.i("MainActivity", CryptoUtils.encrypt())
+        Log.i("MainActivity", CryptoUtils.decrypt("NyWoRYUPmrIl+d1onvDuPgnJs3CxeamuFFeqE3F3PPu++FUPy1/N0w=="))
+    }
+}
